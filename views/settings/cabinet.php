b9fc481d67b43af6c118c6842cd2fe3a8e5b8965
shamshod <shamik1551@mail.ru>
2016-08-30 19:24:36 +0300

    - styles

--- views/settings/cabinet.php
+++ views/settings/cabinet.php
@@ -1,12 +1,11 @@
 <div ng-app="Settings" ng-controller="CabinetsCtrl" ng-init="<?= $ang_init_data ?>">
 	<div class="row" style="position: relative">
         <div class="col-sm-12">
-            <div class="branches" ng-repeat="Branch in Branches">
-                <p><span ng-bind-html="Branch.svg"></span> {{ Branch.name }}</p>
+            <div class="branches" ng-repeat="Branch in Branches" ng-show="Cabinets[Branch.id] && Cabinets[Branch.id].length">
+                <p style="margin-bottom:7px;"><span ng-bind-html="Branch.svg"></span>{{ Branch.name }}</p>
                 <table class="table table-divlike">
                     <tr ng-repeat="Cabinet in Cabinets[Branch.id]">
-                        <td width="200px">{{ Cabinet.number }}</td>
-
+                        <td width="200px" style="padding-left: 20px;">{{ Cabinet.number }}</td>
                         <td width="150">
 							    <span ng-repeat="(day, data) in Cabinet.bar" class="group-freetime-block">
 									<span ng-repeat="bar in data" class="bar {{bar}}"></span>
