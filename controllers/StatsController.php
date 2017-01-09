200db5ecb5cd4c8d0b8e40131dae597874dc9cae
Max Kolyadin <makcyxa-k@yandex.ru>
2017-01-09 15:32:30 +0300

    Payments pagination

--- controllers/StatsController.php
+++ controllers/StatsController.php
@@ -781,13 +781,6 @@
 
 		private function _getPayments($date_start, $date_end = false)
 		{
-            # получаем значение текущей страницы
-            $page = (!empty($_GET['page'])) ? intval($_GET['page']) : 1;
-
-            # получаем указатель с какого по какое загружать
-            $start = ($page - 1) * self::PER_PAGE;
-            $end = $start + self::PER_PAGE;
-
 			$date_start_formatted 	= date("Y-m-d", strtotime($date_start));
 			$date_end_formatted		= date("Y-m-d", strtotime($date_end));
 
@@ -795,7 +788,6 @@
 				"condition" => "entity_type = '" . (isset($_GET['teachers']) ? Teacher::USER_TYPE : Student::USER_TYPE) . "' and ".
 					($date_end 	? "STR_TO_DATE(date, '%d.%m.%Y') > '$date_start_formatted' AND STR_TO_DATE(date, '%d.%m.%Y') <= '$date_end_formatted'"
 								: "date = '$date_start'")
-                "limit" => "{$start}, {$end}"
 			]);
 
 			foreach ($Payments as $Payment) {
