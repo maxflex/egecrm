87ae4ea5939432baf322f576a403a38c7942c83a
Shamshod <shamik1551@mail.ru>
2017-04-25 11:18:39 +0300

    contract saving fixes

--- js/ng-request-app.js
+++ js/ng-request-app.js
@@ -1281,6 +1281,8 @@ app = angular.module("Request", ["ngAnimate", "ngMap", "ui.bootstrap"])
         }
 
         $scope.getContractSum = function(contract) {
+            if (!contract) return 0;
+
             if (contract.discount) {
                 return contract.sum - (contract.sum * (contract.discount / 100))
             } else {
@@ -1448,7 +1450,7 @@ app = angular.module("Request", ["ngAnimate", "ngMap", "ui.bootstrap"])
 			}
 
             // если предмет желтый или зеленый, то поле «кол-во занятий» не может быть пустым или нулем
-            $.each($scope.current_contract.subjects, function(subject_id, subject) {
+            $.each($scope.current_contract.subjects.filter(Boolean), function(subject_id, subject) {
                 if ((subject.status == 2 || subject.status == 3) && !parseInt(subject.count)) {
                     $("#subject-" + subject_id).addClass("has-error").focus()
                     error = true
