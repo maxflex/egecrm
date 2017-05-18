7f60b19806e429a8682ad263bfaa042bc23a555a
Max Kolyadin <makcyxa-k@yandex.ru>
2017-05-18 12:12:28 +0300

    Grade before save

--- models/TeacherReview.php
+++ models/TeacherReview.php
@@ -4,39 +4,39 @@
 		/*====================================== ПЕРЕМЕННЫЕ И КОНСТАНТЫ ======================================*/
 
 		public static $mysql_table	= "teacher_reviews";
-		
+
 		const PER_PAGE = 30;
 		const PLACE = 'REVIEW';
 
 		/*====================================== СИСТЕМНЫЕ ФУНКЦИИ ======================================*/
-		
+
 		public function __construct($array)
 		{
 			parent::__construct($array);
 			if (! $this->isNewRecord) {
 				$this->Student = Student::getLight($this->id_student);
 				$this->lesson_count = VisitJournal::count([
-					'condition' => "id_entity = {$this->id_student} AND id_teacher = {$this->id_teacher} 
+					'condition' => "id_entity = {$this->id_student} AND id_teacher = {$this->id_teacher}
 										AND id_subject = {$this->id_subject} AND year = {$this->year}"
-				]);	
+				]);
 			}
 		}
 
 		/*====================================== СТАТИЧЕСКИЕ ФУНКЦИИ ======================================*/
-		
+
 		public static function countByYear()
 		{
 			$new_search = new StdClass;
 			$new_search->mode = 0;
-			
+
 			$search = json_decode($_COOKIE['reviews']);
 			if ($search->year) {
-				$new_search->year = $search->year;				
+				$new_search->year = $search->year;
 			}
-			
+
 			return static::_count($new_search);
 		}
-		
+
 		public static function addData($rating, $id_student, $id_teacher, $id_subject, $year)
 		{
 			$data = [
@@ -56,9 +56,9 @@
 				"max_score" 			=> $rating['max_score'],
 				"date"					=> now(),
 			];
-			
+
 			$Review = TeacherReview::getInfo($id_student, $id_teacher, $id_subject, $year);
-			
+
 			if ($Review) {
 				$Review->update($data, true);
 			} else {
@@ -73,7 +73,7 @@
 				"condition" => "id_student=" . $id_student . " AND id_subject={$id_subject} AND id_teacher={$id_teacher} AND year={$year}",
 			]);
 		}
-		
+
 
 		/**
 		 * Получить оценку учителя
@@ -90,7 +90,7 @@
 				return 0;
 			}
 		}
-		
+
 		/*
 		 * Получить данные для основного модуля
 		 * $id_student – если просматриваем отзывы отдельного ученика
@@ -102,30 +102,30 @@
 			}
 			// С какой записи начинать отображение, по формуле
 			$start_from = ($page - 1) * TeacherReview::PER_PAGE;
-			
+
 			$search = $id_student ? (object)compact('id_student') : json_decode($_COOKIE['reviews']);
 			if (gettype($search) != "object") {
 				$search = new StdClass;
 			}
 
 			// получаем данные
-			$query = static::_generateQuery($search, "vj.id_entity, vj.id_subject, vj.id_teacher, vj.year, r.id, r.rating, 
+			$query = static::_generateQuery($search, "vj.id_entity, vj.id_subject, vj.id_teacher, vj.year, r.id, r.rating,
 				r.admin_rating, r.admin_rating_final, r.published, r.score, r.max_score, r.comment, r.admin_comment, r.admin_comment_final, " . static::_countQuery('vj2'));
 			$result = dbConnection()->query($query . ($id_student ? "" : " LIMIT {$start_from}, " . TeacherReview::PER_PAGE));
-			
+
 			while ($row = $result->fetch_object()) {
 				$data[] = $row;
 			}
-			
+
 			foreach ($data as &$d) {
 				$d->Student = Student::getLight($d->id_entity);
 				$d->Teacher = Teacher::getLight($d->id_teacher);
 			}
-			
+
 			if (! $id_student) {
 				// counts
 				$counts['all'] = static::_count($search);
-				
+
 				foreach(array_merge([""], Years::$all) as $year) {
 					$new_search = clone $search;
 					$new_search->year = $year;
@@ -173,31 +173,31 @@
 					$counts['user'][$user['id']] = static::_count($new_search);
 				}
 			}
-			
+
 			return [
 				'data' 	=> $data,
 				'counts' => $counts,
 			];
 		}
-		
+
 		private static function _count($search) {
 			return dbConnection()
 					->query(static::_generateQuery($search, "COUNT(*) AS cnt FROM (SELECT vj.id", false, ") AS X"))
 					->fetch_object()
 					->cnt;
 		}
-		
+
 		private static function _connectTables($t, $addon = '') {
 			return " {$t} ON ({$t}.id_student = vj.id_entity AND {$t}.id_teacher = vj.id_teacher AND {$t}.id_subject = vj.id_subject AND {$t}.year = vj.year {$addon})";
 		}
-		
+
 		private static function _countQuery($t) {
 			return "
-				(SELECT COUNT(*) FROM visit_journal {$t} 
-				WHERE {$t}.id_entity = vj.id_entity AND {$t}.id_teacher = vj.id_teacher 
+				(SELECT COUNT(*) FROM visit_journal {$t}
+				WHERE {$t}.id_entity = vj.id_entity AND {$t}.id_teacher = vj.id_teacher
 					AND {$t}.id_subject = vj.id_subject AND {$t}.year = vj.year) AS lesson_count";
 		}
-		
+
 		private static function _generateQuery($search, $select, $order = true, $ending)
 		{
 			$main_query = "
@@ -219,4 +219,18 @@
 				. ($order ? " ORDER BY vj.lesson_date DESC" : "");
 			return "SELECT " . $select . $main_query . $ending;
 		}
+
+
+        public function beforeSave()
+        {
+            if ($this->isNewRecord) {
+                $this->grade = dbConnection()->query("SELECT grade FROM visit_journal vj
+                    WHERE vj.id_teacher = {$this->id_teacher}
+                        AND vj.id_entity = {$this->id_student}
+                        AND vj.id_subject = {$this->id_subject}
+                        AND vj.year = {$this->year}
+                        AND vj.type_entity = 'STUDENT'
+                    LIMIT 1")->fetch_object()->grade;
+            }
+        }
 	}
