b0afa71bd3e128938c80c25b145e8fe2482e88a8
Max Kolyadin <makcyxa-k@yandex.ru>
2017-08-24 11:03:27 +0300

    Column cache cleared

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -13,4 +13,23 @@
             $count = $client->get('egerep-web:constants:TRANSPORT_DISTANCE');
             var_dump($count);
         }
+
+		/**
+		 * Обновление кеша полей таблиц.
+		 */
+		public function actionClearColumnCache()
+		{
+			$Tables = dbConnection()->query("SHOW TABLES");
+			while ($Table = $Tables->fetch_assoc())
+			{
+				$table_name = $Table["Tables_in_".DB_PREFIX."egecrm"];
+				memcached()->delete($table_name."Columns");
+				$Query = dbConnection()->query("SHOW COLUMNS FROM `".$table_name."`");
+				$mysql_vars = [];
+				while ($data = $Query->fetch_assoc()) {
+					$mysql_vars[] = $data["Field"];
+				}
+				memcached()->set($table_name."Columns", $mysql_vars, 3600 * 24);
+			}
+		}
 	}
