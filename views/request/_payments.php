348214eebec0e36e14ca98caf402e2a629774816
Max Kolyadin <makcyxa-k@yandex.ru>
2017-12-04 15:14:19 +0300

    Student payment statuses

--- views/request/_payments.php
+++ views/request/_payments.php
@@ -1,11 +1,9 @@
 <div class="row" ng-show="current_menu == 1">
     <div class="col-sm-12" style='margin-bottom: 10px'>
         <select class='form-control' ng-model='student.payment_status' ng-change='changePaymentStatus()' style='width: 250px'>
-            <option value="0">не обработано</option>
-            <option disabled>──────────────</option>
-            <option value="1">недозвон</option>
-            <option value="2">планирует оплатить</option>
-            <option value="3">оплата внесена</option>
+            <?php foreach(StudentPaymentStatuses::$all as $id => $label) :?>
+                <option value="<?= $id ?>"><?= $label ?></option>
+            <?php endforeach ?>
         </select>
     </div>
     <div class="col-sm-12">
