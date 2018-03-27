68ceb5ca125a02e268e6766468faf3f59d32f5d0
maxflex <makcyxa-k@yandex.ru>
2018-03-27 11:56:47 +0300

    group cache removed

--- views/_partials/_groups_list.php
+++ views/_partials/_groups_list.php
@@ -52,7 +52,7 @@
 -->
 		<td width="15%">
 			<span ng-show="Group.first_schedule">
-				<span ng-show="!Group.past_lesson_count">1-й урок {{Group.first_schedule | date:"dd.MM"}}</span><span ng-show="Group.past_lesson_count">был {{Group.past_lesson_count}} <ng-pluralize count="Group.past_lesson_count" when="{
+				<span ng-show="!Group.past_lesson_count">1-й урок {{Group.first_schedule | date:"dd.MM"}}</span><span ng-show="Group.past_lesson_count">было {{Group.past_lesson_count}} <ng-pluralize count="Group.past_lesson_count" when="{
 					'one': 'урок',
 					'few': 'урока',
 					'many': 'уроков'
