0c92aa85b2ec50ac700eed7de770b6ad1816a4fb
maxflex <makcyxa-k@yandex.ru>
2018-04-10 15:38:11 +0300

    style updates

--- views/_partials/_reviews.php
+++ views/_partials/_reviews.php
@@ -1,6 +1,6 @@
 <table class="table table-hover border-reverse" style='font-size: 12px' <?= ($review_by_year ? ' ng-repeat="review_year in getReviewsYears()"' : '') ?>>
 	<tr class="no-hover" ng-if="review_year">
-		<td colspan="8" class="no-border-bottom">
+		<td colspan="8" class="no-border-bottom" style='padding-top: 0'>
 			<h4 class="row-header">Отзывы {{ review_year + '-' + (review_year + 1) }} учебного года</h4>
 		</td>
 	</tr>
