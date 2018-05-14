f897568427cbb47fbf042bc2241b74673bbfca2b
maxflex <makcyxa-k@yandex.ru>
2018-05-14 11:25:25 +0300

    bg update

--- models/Background.php
+++ models/Background.php
@@ -28,7 +28,7 @@
 	            if (! $wallpaper) {
 					$wallpaper = Background::find([
 						"condition" => "status=1 AND date<CURDATE()",
-						"order" => "id desc"
+						"order" => "date desc"
 					]);
 	            }
 			}
