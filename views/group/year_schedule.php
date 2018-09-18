6334b7eadeb78a49a32dd50a8eca4cbe3298d3f0
maxflex <makcyxa-k@yandex.ru>
2018-09-18 17:32:12 +0300

    Faster schedule load

--- views/group/year_schedule.php
+++ views/group/year_schedule.php
@@ -1,6 +1,6 @@
 <div ng-app="Group" ng-controller="YearCtrl" ng-init="<?= $ang_init_data ?>" style='position: relative; overflow: hidden'>
 	<div class="div-loading" ng-hide='Lessons !== undefined'>
-		<span>загрузка...</span>
+		загрузка...
 	</div>
 	<?= globalPartial('lessons_and_reports') ?>
 </div>
