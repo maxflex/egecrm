a528a77c0fed2ece2eca138f69f23387e1ca2abe
maxflex <makcyxa-k@yandex.ru>
2018-09-18 17:19:44 +0300

    style fixes

--- views/group/year_schedule.php
+++ views/group/year_schedule.php
@@ -1,6 +1,6 @@
-<div ng-app="Group" ng-controller="YearCtrl" ng-init="<?= $ang_init_data ?>">
+<div ng-app="Group" ng-controller="YearCtrl" ng-init="<?= $ang_init_data ?>" style='position: relative'>
 	<div class="div-loading" ng-hide='true'>
-		загрузка...
+		<span>загрузка...</span>
 	</div>
 	<?= globalPartial('lessons_and_reports') ?>
 </div>
